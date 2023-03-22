88e994a16c464edbeb26754f6676fc329cd661a1
`` MOUSAB <mal41@smail.ucas.edu.ps>
2023-03-22 20:43:37 +0200

    asd

--- app/src/main/java/com/example/car/IntroductionActivity.java
+++ app/src/main/java/com/example/car/IntroductionActivity.java
@@ -2,11 +2,17 @@ package com.example.car;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.AnimationUtils;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.example.car.Screen_Login_Register.LoginActivity;
 import com.example.car.databinding.ActivityIntroductionBinding;
+
+import java.util.ArrayList;
 
 public class IntroductionActivity extends AppCompatActivity {
 
@@ -22,8 +28,10 @@ public class IntroductionActivity extends AppCompatActivity {
         binding.tvDetailsIntro.startAnimation(AnimationUtils.loadAnimation(IntroductionActivity.this, R.anim.fadein));
         binding.linearWelcome.startAnimation(AnimationUtils.loadAnimation(IntroductionActivity.this, R.anim.fadein_fast));
 
+        ArrayList<String> arrayList = new ArrayList<>();
         binding.btnNext.setOnClickListener(view -> {
-
+            startActivity(new Intent(getBaseContext(), LoginActivity.class));
+            finish();
         });
     }
 }
