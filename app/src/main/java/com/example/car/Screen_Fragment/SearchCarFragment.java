18c1f902fbb98611f8daaf8db26c47d1075462ab
`` MOUSAB <mal41@smail.ucas.edu.ps>
2023-03-26 07:04:56 +0300

    home_addPost

--- app/src/main/java/com/example/car/Screen_Fragment/SearchCarFragment.java
+++ app/src/main/java/com/example/car/Screen_Fragment/SearchCarFragment.java
@@ -1,36 +1,24 @@
 package com.example.car.Screen_Fragment;
 
-import static android.app.Activity.RESULT_OK;
-
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.car.Adapter.SelectImageAdapter;
 import com.example.car.R;
-import com.example.car.databinding.FragmentAddPostBinding;
-
-import java.util.ArrayList;
+import com.example.car.databinding.FragmentSearchBinding;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AddPostFragment#newInstance} factory method to
+ * Use the {@link SearchCarFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AddPostFragment extends Fragment{
+public class SearchCarFragment extends Fragment {
 
-    FragmentAddPostBinding binding;
+    FragmentSearchBinding binding;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -38,7 +26,7 @@ public class AddPostFragment extends Fragment{
     private String mParam1;
     private String mParam2;
 
-    public AddPostFragment() {
+    public SearchCarFragment() {
         // Required empty public constructor
     }
 
@@ -48,11 +36,11 @@ public class AddPostFragment extends Fragment{
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment SearchCarFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AddPostFragment newInstance(String param1, String param2) {
-        AddPostFragment fragment = new AddPostFragment();
+    public static SearchCarFragment newInstance(String param1, String param2) {
+        SearchCarFragment fragment = new SearchCarFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -72,7 +60,7 @@ public class AddPostFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentAddPostBinding.inflate(inflater);
+        binding = FragmentSearchBinding.inflate(inflater);
         return binding.getRoot();
     }
 }
--- app/src/main/java/com/example/car/Screen_Home/HomeActivity.java
+++ app/src/main/java/com/example/car/Screen_Home/HomeActivity.java
@@ -1,17 +1,30 @@
 package com.example.car.Screen_Home;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.car.Adapter.CarAdapter;
 import com.example.car.Adapter.WelcomeAdapter;
 import com.example.car.Model.Car;
 import com.example.car.R;
 import com.example.car.Screen_Fragment.AddPostFragment;
+import com.example.car.Screen_Fragment.HomeFragment;
 import com.example.car.databinding.ActivityHomeBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -36,16 +49,49 @@ public class HomeActivity extends AppCompatActivity {
         for (int i = 0; i < count; i++) {
             yearsList.add(Integer.toString(current_year - i));
         }
-        ArrayAdapter<String> yearsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, yearsList);
+        ArrayAdapter<String> yearsAdapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_spinner_item, yearsList);
         binding.spinnerYear.setAdapter(yearsAdapter);
 
         arrayList = new ArrayList<>();
+        arrayList.add(new HomeFragment());
         arrayList.add(new AddPostFragment());
-//        arrayList.add(new SearchFragment());
-//        arrayList.add(new UsersFragment());
+        arrayList.add(new AddPostFragment());
+        arrayList.add(new AddPostFragment());
 
         adapter = new WelcomeAdapter(HomeActivity.this, arrayList);
         binding.viewPagerHome.setAdapter(adapter);
         binding.viewPagerHome.setUserInputEnabled(false);
+
+        binding.bottomNavigationHome.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.home_nav) {
+                    binding.viewPagerHome.setCurrentItem(0);
+                } else if (itemId == R.id.search_nav) {
+                    binding.viewPagerHome.setCurrentItem(1);
+                } else if (itemId == R.id.add_nav) {
+                    binding.viewPagerHome.setCurrentItem(0);
+                } else if (itemId == R.id.buy_nav) {
+                    binding.viewPagerHome.setCurrentItem(3);
+                }else if (itemId == R.id.user_nav) {
+                    binding.viewPagerHome.setCurrentItem(4);
+                }
+                return false;
+            }
+        });
+
+        binding.viewPagerHome.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+               binding.bottomNavigationHome.getMenu().getItem(position).setChecked(true);
+            }
+        });
+
+        binding.imageAddPost.setOnClickListener(v -> {
+            startActivity(new Intent(getBaseContext(),AddPostActivity.class));
+        });
     }
 }
